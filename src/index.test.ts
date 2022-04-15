92e0dbc10d6557ff8b37fb9f2d27f7053d31262c
srbitgithub <sr_bit@hotmail.com>
2022-04-15 17:29:28 +0200

    MMM command added

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,11 +1,16 @@
-import { test } from "./index"
+import { MarsRover } from "./index"
 
-describe("Kata-TS", () => {
-  it("returns 'That works fine'", () => {
-    const expected = "That works fine";
-
-    const result = test();
-
-    expect(result).toEqual(expected);
+describe("Mars Rover", () => {
+  it("If command to move is empty, returns same coords.", () => {
+    const expected = "5,5,N";
+    const marsRover = new MarsRover("5,5,N", "");
+    expect(marsRover.result).toEqual(expected);
   })
+
+  it("When command to move is M MarsRover advance one by M", () => {
+    const expected = "1,5,N";
+    const marsRover = new MarsRover("1,2,N", "MMM");
+    expect(marsRover.result).toEqual(expected);
+  })
+
 })
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,58 @@
-export function test(): any {
-    return "That works fine"
-  };
+type Coordinates = {
+  x: number,
+  y: number,
+  direction: string
+}
+
+export class MarsRover {
+  landingCoords:string = ""
+  commandMovement:string = ""
+  result:string = ""
+  currentPosition:Coordinates = {x:0,y:0,direction:"N"}
+
+  constructor(_landingCoords:string, _commandMovement:string){
+    this.landingCoords = _landingCoords
+    this.commandMovement = _commandMovement
+
+    this.initialCoords()
+    this.main()
+  }
+
+  main (){
+    var commandMovementArray:string[]
+    if(this.commandMovement === "") this.result = this.landingCoords
+    if (this.commandMovement === "MMM") 
+    {
+      commandMovementArray = this.convertStringToArray(this.commandMovement)
+      commandMovementArray.forEach(item => {
+        this.updateLocation(item)
+      });
+      this.updateResult()
+    }
+  }
+
+  initialCoords(){
+    var stringToConvertArray:string[]
+    stringToConvertArray = this.convertStringToArrayByComas(this.landingCoords)
+    this.currentPosition.x = parseInt(stringToConvertArray[0])
+    this.currentPosition.y = parseInt(stringToConvertArray[1])
+    this.currentPosition.direction = stringToConvertArray[2]
+  }
   
+  
+  convertStringToArray(stringToConvert:string):string[]{
+    return stringToConvert.split('')
+  }
+  
+  convertStringToArrayByComas(stringToConvert:string):string[]{
+    return stringToConvert.split(',')
+  }
+  
+  updateLocation(item:string){
+    if(item === 'M') this.currentPosition.y++
+  }
+  
+  updateResult(){
+    this.result = `${this.currentPosition.x.toString()},${this.currentPosition.y.toString()},${this.currentPosition.direction}`
+  }
+}
